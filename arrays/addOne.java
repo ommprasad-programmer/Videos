8676ba85c76ea03c6200b77ba8c1f2d86a0c752f
ommprasad-programmer <nathommprasad@gmail.com>
2026-01-16 20:55:59 +0530

    code

---
+++ arrays/addOne.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class addOne {
+    public static void main(String[] args) {
+         int arr [] = {1,2,9,9};
+    for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+   
+}
