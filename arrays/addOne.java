938f5e10157b6914555c9d27d4eb4ffbee11a2c3
ommprasad-programmer <nathommprasad@gmail.com>
2026-01-18 22:08:06 +0530

    modified

--- arrays/addOne.java
+++ arrays/addOne.java
@@ -1,12 +1,24 @@
-package arrays;
+
 
 public class addOne {
+ 
     public static void main(String[] args) {
-         int arr [] = {1,2,9,9};
-    for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        int[] arr = {1, 2, 9};
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++;
+                break;
+            }
+            arr[i] = 0;
         }
 
+     
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
-   
 }
+
+   
+
