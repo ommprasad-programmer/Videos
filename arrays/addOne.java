b957cd4fb7d3b23ab3236836f23d87d3c8a5ba43
ommprasad-programmer <nathommprasad@gmail.com>
2026-01-29 20:03:07 +0530

    newfolder

--- arrays/addOne.java
+++ arrays/addOne.java
@@ -3,7 +3,7 @@
 public class addOne {
  
     public static void main(String[] args) {
-        int[] arr = {1, 2, 9};
+        int[] arr = {1, 2, 9}; 
 
         for (int i = arr.length - 1; i >= 0; i--) {
             if (arr[i] < 9) {
