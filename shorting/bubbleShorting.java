2526f7651f87dd81f149ec42a58b05460a804cd2
ommprasad-programmer <nathommprasad@gmail.com>
2026-02-01 18:55:59 +0530

    modified bubble shot

--- shorting/bubbleShorting.java
+++ shorting/bubbleShorting.java
@@ -1,26 +1,37 @@
-package shorting;
-
 public class bubbleShorting {
+
     public static void main(String[] args) {
-        int [] arr = {1,4,5,3,7,2};
-        int i ; int j ;
-       for (i=0;i<arr.length-1;i++){
-        for (j=0;j<arr.length-1-i;j++){
-            if (arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+
+        int[] arr = {1, 4, 5, 3, 7, 2};
+        int n = arr.length;
+
+        boolean swapped;
+
+        // Bubble Sort logic
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
             }
 
+           
+            if (!swapped) {
+                break;
+            }
         }
 
-       }
-
-        for (int k = 0; k < arr.length; k++) {
-            System.out.print(arr[k] + " ");
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
-
-
-    
 }
