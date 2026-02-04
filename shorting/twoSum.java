80f14e69eb31efc16e435b5f9feb99980367e8a6
ommprasad-programmer <nathommprasad@gmail.com>
2026-02-04 18:30:44 +0530

    twoSum

---
+++ shorting/twoSum.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class twoSum {
+    public static void main(String[] args) {
+
+        int[] arr = {1, -3, 10, 13, 17, 20};
+        int target = -2;
+
+        Arrays.sort(arr);   
+        int n = arr.length;
+
+        int i = 0;
+        int j = n - 1;      
+        boolean twosum = false;
+
+        while (i < j) {
+            int sum = arr[i] + arr[j];
+
+            if (sum == target) {
+                twosum = true;
+                break;
+            } 
+            else if (sum > target) {
+                j--;
+            } 
+            else if (sum < target) {
+                i++;
+            } 
+        
+            
+        }
+
+        if (twosum == true ){
+             System.out.println("Indexes: [" + i + ", " + j + "]");
+             System.out.println("Values: [" + arr[i] + ", " + arr[j] + "]");
+        }
+        else System.out.println("There is no elements exist");
+    }
+}
