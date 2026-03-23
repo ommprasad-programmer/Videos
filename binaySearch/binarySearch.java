1934c8bd15774021a3b32c7b358ea575e3e05d6f
ommprasad-programmer <nathommprasad@gmail.com>
2026-03-23 21:55:36 +0530

    update binarySearch implementation and add firstAndLastOccurrence class

--- binaySearch/binarySearch.java
+++ binaySearch/binarySearch.java
@@ -3,7 +3,7 @@ package binaySearch;
 public class binarySearch {
 
     public static void main(String[] args) {
-        int arr [] = {1,2,3,4,5,6,8,9,10};
+        int arr [] = {1,5,5,5,5,8,9,10};
          int tar = 2;
 
          int n = arr.length ;
---
+++ binaySearch/firstAndLastOcurance.java
@@ -0,0 +1,54 @@
+package binaySearch;
+
+public class firstAndLastOcurance {
+    public static void main(String[] args) {
+        int arr[] = {2,4,5,5,5,5,5,6,7};
+        int target = 5;
+
+        int firstidx = findFirst(arr, target);
+        int lastidx = findLast(arr, target);
+
+        System.out.println("First Index: " + firstidx);
+        System.out.println("Last Index: " + lastidx);
+    }
+
+ 
+    static int findFirst(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1; 
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    
+    static int findLast(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
