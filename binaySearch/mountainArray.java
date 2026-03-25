b765d088cc1f386cddb574a5ec880e40d01ef769
ommprasad-programmer <nathommprasad@gmail.com>
2026-03-25 22:54:57 +0530

    add mountainArray class to find peak elements in an array

--- binaySearch/mountainArray.java
+++ binaySearch/mountainArray.java
@@ -3,5 +3,14 @@ package binaySearch;
 public class mountainArray {
     public static void main(String[] args) {
         int arr[] = {1,3,4,5,7,9,20,9,8,7};
+        int i;
+       for(i=0;i<arr.length-1;i++){
+          
+            if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
+                System.out.println(arr[i]);
+            }
+
+        }
     }
 }
+ 
