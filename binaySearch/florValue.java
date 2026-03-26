4319c4e963275b71a8226b6939dcfd3d2f6a0e21
ommprasad-programmer <nathommprasad@gmail.com>
2026-03-26 22:08:34 +0530

    add florValue class to implement binary search for finding the floor value in a sorted array

---
+++ binaySearch/florValue.java
@@ -0,0 +1,35 @@
+package binaySearch;
+
+public class florValue {
+    public static void main(String[] args) {
+        int arr[]={2,4,5,6,6,8,9,10};
+        int x=7;
+        int idx=-1;
+
+        int low=0,hi=arr.length-1;
+
+        while(low<=hi){
+            int mid = (hi+low)/2;
+            if(arr[mid]==x){
+
+                idx = mid;
+                break;
+
+            }
+            else if(arr[mid]>x){
+                hi = mid-1;
+            }
+            else{
+                idx = mid ;
+                low = mid+1;
+            }
+            
+
+            
+
+                    
+        }
+
+        System.out.println(idx);
+    }
+}
