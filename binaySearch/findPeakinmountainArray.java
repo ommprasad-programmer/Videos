8558ddacb605053a28b85a5022b727e7ebb462c5
ommprasad-programmer <nathommprasad@gmail.com>
2026-03-26 19:59:59 +0530

    refactor mountainArray class to implement binary search for peak elements

--- binaySearch/findPeakinmountainArray.java
+++
@@ -1,7 +0,0 @@
-package binaySearch;
-
-public class findPeakinmountainArray {
-    public static void main(String[] args) {
-        
-    }
-}
--- binaySearch/mountainArray.java
+++ binaySearch/mountainArray.java
@@ -2,15 +2,28 @@ package binaySearch;
 
 public class mountainArray {
     public static void main(String[] args) {
-        int arr[] = {1,3,4,5,7,9,20,9,8,7};
-        int i;
-       for(i=0;i<arr.length-1;i++){
-          
-            if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
-                System.out.println(arr[i]);
+        int arr[] = { 1, 3, 4, 5, 7, 9, 20, 9, 8, 7 };
+        int hi = arr.length - 2;
+        int low = 1;
+
+        while (hi > low) {
+
+            int mid = (low + hi) / 2;
+
+            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+
+                System.out.println(mid);
+                break;
             }
 
+            else if (arr[mid - 1] < arr[mid] && arr[mid + 1] > arr[mid]) {
+
+                low = mid + 1;
+
+            } else
+                hi = mid - 1;
+
         }
+
     }
 }
- 
