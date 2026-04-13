44f05cbbbb6c7e66de1ba7bdcbd9f712a86c21ae
ommprasad-programmer <nathommprasad@gmail.com>
2026-04-13 23:06:54 +0530

    add floar_sqrt class to implement binary search for calculating the floor square root of a number

---
+++ binaySearch/floar_sqrt.java
@@ -0,0 +1,28 @@
+package binaySearch;
+
+public class floar_sqrt {
+
+    public static int floorSqrt(int n) {
+        if (n == 0 || n == 1) return n;
+
+        int low = 1, high = n, ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+          
+            if (mid <= n / mid) {
+                ans = mid;        
+                low = mid + 1;    
+            } else {
+                high = mid - 1;   
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(floorSqrt(n)); 
+    }
+}
