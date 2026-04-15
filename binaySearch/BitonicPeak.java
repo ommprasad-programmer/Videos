e31a58423a8c783816e02b6edb19517254234163
ommprasad-programmer <nathommprasad@gmail.com>
2026-04-15 22:55:30 +0530

    add BitonicPeak class to implement peak finding in a bitonic array

---
+++ binaySearch/BitonicPeak.java
@@ -0,0 +1,26 @@
+public class BitonicPeak {
+
+    public static int findPeak(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                // Peak is on the right side
+                low = mid + 1;
+            } else {
+                // Peak is on the left side or at mid
+                high = mid;
+            }
+        }
+
+        return arr[low]; // or arr[high]
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 12, 4, 2};
+        System.out.println("Peak element: " + findPeak(arr));
+    }
+}
