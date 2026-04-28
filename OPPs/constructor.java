4413ac28600b7f6c6ca7d2336e087005f388cbcc
ommprasad-programmer <nathommprasad@gmail.com>
2026-04-28 23:37:56 +0530

    refactor constructor class to define Person with attributes and methods

--- OPPs/constructor.java
+++ OPPs/constructor.java
@@ -1,5 +1,21 @@
 package DSA.OPPs;
 
-public class constructor {
-    
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println(name + " is " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Alice", 25);
+        p1.display();
+    }
 }
