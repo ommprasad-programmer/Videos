b67e919033f68da945ccfaaf6d4d9b608fe19c25
ommprasad-programmer <nathommprasad@gmail.com>
2026-06-03 23:39:34 +0530

    refactor lc5 class main method to improve array filtering logic

--- leetcode/lc5.java
+++ leetcode/lc5.java
@@ -4,33 +4,24 @@ public class lc5 {
 
     public static void main(String[] args) {
 
-        int arr[] = {1,2,3,2,4,5}; int brr[]={};
+       int[] arr = {1,2,3,2,4,5};
+int val = 2;
 
-       int n = arr.length; int val =2;
+int k = 0;
 
-       for(int i =0;i<n-1;i++){
-         
-
-        if(arr[i]!=val){
-            arr[i]=brr[i];
-        }
-        
-        if(arr[i]==val){
-            continue;
-         }
-       }
-
-       print(brr);
-        
+for (int i = 0; i < arr.length; i++) {
+    if (arr[i] != val) {
+        arr[k] = arr[i];
+        k++;
     }
+}
 
-  public static void print (int []arr){
+System.out.println("k = " + k);
 
-    for(int i=0;i<=arr.length-1;i++){
-        System.out.println(arr[i]);
-    }
 
-  }
 
+ 
     
+}
+
 }
